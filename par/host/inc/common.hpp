d5f3b29a09341ed5106f9c7ea5c62346cbb2914f
blackwut <albertoottimo@gmail.com>
2021-01-11 16:42:52 +0100

    Brand new! Contains only cout shortcuts

--- par/host/inc/common.hpp
+++ par/host/inc/common.hpp
@@ -1,94 +1,10 @@
 #pragma once
 
-#define TUPLE_SIZE              4
-#define COMPUTE_UNITS           8
+#define COUT_STRING_W   24
+#define COUT_DOUBLE_W   8
+#define COUT_INTEGER_W  8
+#define COUT_PRECISION  3
 
-enum K_TYPE {
-    K_BASE,
-    K_UNROLL,
-    K_REPLICA,
-    K_REPLICA_NEW,
-    K_NDRANGE,
-    K_FUSE
-};
-
-#define K_BASE_NUMS           3
-const char * K_BASE_NAMES[] {
-    "source",
-    "map",
-    "sink"
-};
-const char * P_BASE_FILENAME = "./base.aocx";
-
-
-#define K_UNROLL_NUMS           3
-const char * K_UNROLL_NAMES[] {
-    "source",
-    "map",
-    "sink"
-};
-const char * P_UNROLL_FILENAME = "./unroll.aocx";
-
-
-#define K_REPLICA_NUMS          10
-const char * K_REPLICA_NAMES[] {
-    "source",
-    "map0",
-    "map1",
-    "map2",
-    "map3",
-    "map4",
-    "map5",
-    "map6",
-    "map7",
-    "sink"
-};
-const char * P_REPLICA_FILENAME = "./replica.aocx";
-
-#define K_REPLICA_NEW_NUMS          24
-const char * K_REPLICA_NEW_NAMES[] {
-    "source0",
-    "source1",
-    "source2",
-    "source3",
-    "source4",
-    "source5",
-    "source6",
-    "source7",
-    "map0",
-    "map1",
-    "map2",
-    "map3",
-    "map4",
-    "map5",
-    "map6",
-    "map7",
-    "sink0",
-    "sink1",
-    "sink2",
-    "sink3",
-    "sink4",
-    "sink5",
-    "sink6",
-    "sink7"
-};
-const char * P_REPLICA_NEW_FILENAME = "./replica_new.aocx";
-
-
-#define K_NDRANGE_NUMS          3
-const char * K_NDRANGE_NAMES[] {
-    "source",
-    "map",
-    "sink"
-};
-const char * P_NDRANGE_FILENAME = "./ndrange.aocx";
-
-
-#define K_FUSE_NUMS             3
-const char * K_FUSE_NAMES[] {
-    "source",
-    "map",
-    "sink"
-};
-const char * P_FUSE_FILENAME = "./fuse.aocx";
-
+#define COUT_HEADER     std::setw(COUT_STRING_W) << std::right
+#define COUT_FLOAT      std::setw(COUT_DOUBLE_W) << std::right << std::fixed << std::setprecision(COUT_PRECISION)
+#define COUT_INTEGER    std::setw(COUT_INTEGER_W) << std::right << std::fixed
